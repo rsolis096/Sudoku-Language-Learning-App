533b42c143d5cea3055d859090e948a5055ec268
rds11 <rds11@sfu.ca>
2023-01-15 17:34:40 -0800

    Created a basic table and implemented a function and button to retrieve values from specific boxes.

--- app/src/main/java/com/example/sudokuapp/MainActivity.java
+++ app/src/main/java/com/example/sudokuapp/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.sudokuapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TableRow;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        //Get value at index 2 (indexing starts at 0)
+        TableRow rowOne = (TableRow) findViewById(R.id.row1);
+
+        //Convert that value to a text view object
+        TextView items = (TextView) rowOne.getChildAt(2);
+
+        //Button used to display the value
+        Button btn = findViewById(R.id.bttnClickMe);
+
+        //Behaviour of button on click
+        btn.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                Log.i("Test"+items.getText().toString(), "banana");
+                onButtonClick(items.getText().toString());
+
+            }
+        });
     }
+
+    protected void onButtonClick(String myNum)
+    {
+        Log.i("Test" + myNum, "onbclick func");
+        Toast.makeText(this, myNum,Toast.LENGTH_SHORT);
+    }
+
 }
