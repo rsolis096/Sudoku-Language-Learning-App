750653b9ef4bcf22590a919b7a33e9cf248c80f7
Ricardo Solis <rds11@sfu.ca>
2023-03-21 23:00:50 -0700

    Added File I/O
    Some basic FILE I/O functionality added to the main menu options page

--- app/src/main/java/com/example/sudokuapp/OptionsPage.java
+++ app/src/main/java/com/example/sudokuapp/OptionsPage.java
@@ -5,8 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class OptionsPage extends AppCompatActivity {
 
@@ -18,6 +25,24 @@ public class OptionsPage extends AppCompatActivity {
         // close this activity
         setupBackToMain();
 
+        // Update Text box with file contents
+        try {
+            readFile();
+        }
+        catch (IOException e) {
+            throw new RuntimeException("DOESNT EXIST",e);
+
+        }
+
+        //Setup buttons
+        try {
+            setupCustomWordsButton();
+            setupWriteButton();
+        }
+        catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
         // theme/colour
         // leaderboard
         // modify word bank
@@ -28,6 +53,73 @@ public class OptionsPage extends AppCompatActivity {
         Button btn = findViewById(R.id.optionsBackToMenu);
         btn.setOnClickListener(view -> finish());
     }
+
+    private void setupCustomWordsButton() throws IOException {
+        Button btnCustom = findViewById(R.id.btnCustom);
+        btnCustom.setOnClickListener(view -> {
+            try {
+                readFile();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
+    }
+
+    private void setupWriteButton() throws IOException {
+        Button btnWrite= findViewById(R.id.btnWrite);
+        btnWrite.setOnClickListener(view -> {
+            try {
+                writeToFile();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
+    }
+
+    private void readFile() throws IOException {
+
+        String fileName = "myOtherText.txt";
+        File file = new File(getFilesDir(), fileName);
+
+        //create the file if it doesn't exist
+        if(!file.exists()) {
+
+            FileOutputStream outputStream = openFileOutput(fileName, Context.MODE_PRIVATE);
+        }
+
+        FileInputStream inputStream = openFileInput(fileName);
+        //Initialize a byte array
+        byte[] buffer = new byte[inputStream.available()];
+
+        //Fill byte array with contents of file (file contents are in byte form)
+        inputStream.read(buffer);
+
+        //Use built in String constructor to convert byte array into string
+        String fileContents = new String(buffer);
+
+        //Close file
+        inputStream.close();
+
+        //Write the string to TextView
+        TextView myText = findViewById(R.id.displayTextView);
+        myText.setText(fileContents);
+
+        //Confirmation message
+        Toast toast = Toast.makeText(findViewById(R.id.btnWrite).getContext(), "CONTENTS WRITTEN ON SCREEN",Toast.LENGTH_LONG);
+        toast.show();
+    }
+
+    private void writeToFile() throws IOException {
+        FileOutputStream myFileOut = openFileOutput("myOtherText.txt", MODE_PRIVATE);
+        EditText editTextBox = findViewById(R.id.readEditTextView);
+        myFileOut.write(editTextBox.getText().toString().getBytes());
+        myFileOut.close();
+
+        Toast toast = Toast.makeText(findViewById(R.id.btnWrite).getContext(), "FILE CONTENTS UPDATED",Toast.LENGTH_LONG);
+        toast.show();
+
+    }
+
 
     public static Intent makeIntent(Context context) {
         return new Intent(context, OptionsPage.class);
