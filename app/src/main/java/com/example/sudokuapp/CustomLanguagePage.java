54efef583744259c622bc7ef849b51bcff295edd
rds11 <rds11@sfu.ca>
2023-03-22 14:15:09 -0700

    Add File I/O
    User can now write pairs of words to a text file. The contents of the file are also displayed on screen.

--- app/src/main/java/com/example/sudokuapp/CustomLanguagePage.java
+++ app/src/main/java/com/example/sudokuapp/CustomLanguagePage.java
@@ -18,6 +18,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Objects;
@@ -36,11 +37,26 @@ public class CustomLanguagePage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_custom_language);
 
+        //Fill page with file contents
+        try {
+            //Read the file and update the table layout
+            readFile();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
         //Setup back button
         setupBackToMain();
 
         //Setup Add Row Button
         setupAddRow();
+
+        //Setup Clear Button
+        try {
+            setupClear();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 
@@ -49,30 +65,6 @@ public class CustomLanguagePage extends AppCompatActivity {
         Button btn = findViewById(R.id.btnCustomWordsBack);
         btn.setOnClickListener(view -> finish());
     }
-
-    /*
-    private void setupAddRow(){
-        Button btnCustom = findViewById(R.id.btnCustomWordsAdd);
-        btnCustom.setOnClickListener(view -> {
-            //Increase row amount
-            rowCounter++;
-            //Get reference to Table Layout
-            TableLayout tableLayout = findViewById(R.id.customWordsTable);
-            //Create a new table row object
-            TableRow tableRow = new TableRow(this);
-            //Sets its Content Description for testing
-            tableRow.setContentDescription("customWordsTableRow" + rowCounter);
-
-            //Create the content to be placed in the row
-            TextView insertTextView = new TextView(this);
-            insertTextView.setText("BANANA");
-            tableRow.addView(insertTextView);
-
-            //This adds the created row into the table
-            tableLayout.addView(tableRow);
-        });
-    }
-     */
 
     private void setupAddRow(){
         Button btnCustom = findViewById(R.id.btnCustomWordsAdd);
@@ -100,7 +92,6 @@ public class CustomLanguagePage extends AppCompatActivity {
             userInputLayout.addView(userInputRowOne);
             userInputLayout.addView(userInputRowTwo);
 
-
             //Set up English EditText
             EditText englishInput = new EditText(view.getContext());
             englishInput.setInputType(InputType.TYPE_CLASS_TEXT);
@@ -125,9 +116,14 @@ public class CustomLanguagePage extends AppCompatActivity {
 
                 //Create the content to be placed in the row
                 TextView insertTextView = new TextView(this);
-                String displayString = userEnglishInput +", " + userSpanishInput;
+                String displayString = userEnglishInput +", " + userSpanishInput + "\n";
                 insertTextView.setText(displayString);
                 tableRow.addView(insertTextView);
+                try {
+                    writeToFile(displayString);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
             });
 
             //Set up popup cancel button
@@ -149,8 +145,7 @@ public class CustomLanguagePage extends AppCompatActivity {
 
         //create the file if it doesn't exist
         if(!file.exists()) {
-
-            FileOutputStream outputStream = openFileOutput(fileName, Context.MODE_PRIVATE);
+            FileOutputStream outputStream = openFileOutput(fileName, MODE_PRIVATE);
         }
 
         FileInputStream inputStream = openFileInput(fileName);
@@ -162,28 +157,77 @@ public class CustomLanguagePage extends AppCompatActivity {
 
         //Use built in String constructor to convert byte array into string
         String fileContents = new String(buffer);
+        //Write the string to TableLayout to individual rows
+        String[] fileContentsLines = fileContents.split("\\n");
 
         //Close file
         inputStream.close();
 
-        //Write the string to TextView
-        TextView myText = findViewById(R.id.displayTextView);
-        myText.setText(fileContents);
+        //Get a reference to the Table Layout
+        TableLayout tableLayout = findViewById(R.id.customWordsTable);
+
+        for(int i = 0; i < fileContentsLines.length; i++)
+        {
+            //Create a row to add to the table layout
+            TableRow tableRow = new TableRow(this);
+            tableLayout.addView(tableRow);
+            //Create a text view to add to the row
+            TextView fileLine = new TextView(this);
+            fileLine.setText(fileContentsLines[i]);
+            //Add to table row
+            tableRow.addView(fileLine);
+        }
 
         //Confirmation message
-        Toast toast = Toast.makeText(findViewById(R.id.btnWrite).getContext(), "CONTENTS WRITTEN ON SCREEN",Toast.LENGTH_LONG);
+        Toast toast = Toast.makeText(this, "CONTENTS WRITTEN ON SCREEN",Toast.LENGTH_LONG);
         toast.show();
     }
 
-    private void writeToFile() throws IOException {
-        FileOutputStream myFileOut = openFileOutput("myOtherText.txt", MODE_PRIVATE);
-        EditText editTextBox = findViewById(R.id.readEditTextView);
-        myFileOut.write(editTextBox.getText().toString().getBytes());
+    private void writeToFile(String fileContents) throws IOException {
+        //Open file
+        FileOutputStream myFileOut = openFileOutput("myOtherText.txt", MODE_PRIVATE | MODE_APPEND);
+
+        //Write fileContents to the opened file
+        myFileOut.write(fileContents.getBytes());
+
+        //Close file
         myFileOut.close();
 
-        Toast toast = Toast.makeText(findViewById(R.id.btnWrite).getContext(), "FILE CONTENTS UPDATED",Toast.LENGTH_LONG);
+        //Confirmation toast
+        Toast toast = Toast.makeText(this, "FILE CONTENTS UPDATED",Toast.LENGTH_LONG);
         toast.show();
+    }
 
+    private void setupClear() throws FileNotFoundException
+    {
+        Button btn = findViewById(R.id.btnCustomWordsClear);
+        btn.setOnClickListener(view -> {
+            //Open file
+            FileOutputStream myFileOut = null;
+            try {
+                myFileOut = openFileOutput("myOtherText.txt", MODE_PRIVATE);
+            } catch (FileNotFoundException e) {
+                throw new RuntimeException(e);
+            }
+
+            //Write fileContents to the opened file
+            try {
+                myFileOut.write("".getBytes());
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
+            //Close file
+            try {
+                myFileOut.close();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
+            //Confirmation toast
+            Toast toast = Toast.makeText(this, "FILE CONTENTS UPDATED",Toast.LENGTH_LONG);
+            toast.show();
+        });
     }
 
 
