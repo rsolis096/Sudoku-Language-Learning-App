9d54263c939f68e0156dc97e298b647094ed84db
Ricardo Solis <rds11@sfu.ca>
2024-01-04 21:06:48 -0800

    Fix tts issues

--- app/src/main/java/com/example/sudokuapp/model/Sound.java
+++ app/src/main/java/com/example/sudokuapp/model/Sound.java
@@ -2,30 +2,46 @@ package com.example.sudokuapp.model;
 
 import android.content.Context;
 import android.speech.tts.TextToSpeech;
+import android.util.Log;
 
-import com.example.sudokuapp.model.Sudoku;
 
 import java.util.Locale;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class Sound {
-    private static TextToSpeech tts;
     private static boolean audioMode = false;
+    private static TextToSpeech tts;
     public static int[] playSound(Context context, String wordToSpeak)
     {
         //These are used for unit testing, to verify sound is played and language is set
         AtomicInteger language = new AtomicInteger();
         AtomicInteger spoken = new AtomicInteger();
-
-        tts = new TextToSpeech(context, status -> {
-            if(Sudoku.getTranslationDirection())
+        if(tts != null)
+        {
+            if (Sudoku.getTranslationDirection()) {
                 language.set(tts.setLanguage(new Locale("es", "ME")));
-            else
+            } else {
                 language.set(tts.setLanguage(new Locale("en", "US")));
+            }
+        }
+        else {
+            tts = new TextToSpeech(context, status -> {
+                if (status == TextToSpeech.SUCCESS) {
+                    Log.e("TTS", "TextToSpeech initialized successfully with status: " + status);
+                    if (Sudoku.getTranslationDirection()) {
+                        tts.setLanguage(new Locale("es", "ME"));
+                    } else {
+                        tts.setLanguage(new Locale("en", "US"));
+                    }
+                }
+                else{
+                    Log.e("TTS", "TextToSpeech initialization failed with status: " + status);
+                }
+            });
+        }
+        if(tts != null)
             spoken.set(tts.speak(wordToSpeak, TextToSpeech.QUEUE_FLUSH, null, ""));
-        });
-        //System.out.println(Arrays.toString(Locale.getISOCountries()));
-        tts.shutdown();
+
        return new int[]{language.get(), spoken.get()};
     }
 
